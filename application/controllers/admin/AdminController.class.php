2408d30d4e189e33f82bf6dd9cdb0651f1da9647
Saramiaa <saraa-z@hotmail.fr>
2020-01-06 10:11:11 +0100

    fix conflict

--- application/controllers/admin/AdminController.class.php
+++ application/controllers/admin/AdminController.class.php
@@ -14,11 +14,7 @@ class AdminController
 
       $users = $userModel->getAllUsers();
 
-<<<<<<< HEAD
-      // var_dump($users);
-=======
-      var_dump($users);
->>>>>>> cd9907ce01b9cc906351658d214842debc1e9d66
+
 
     return [
       // for each pour Affichage
